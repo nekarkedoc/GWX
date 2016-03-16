0dc2dd8075cd2ee3f32902b3ab64f0e6f18a2675
Steven W. Scott <codekraken@gmail.com>
2016-03-16 18:35:13 -0400

    added to repository

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            String a1 = args[0];
+//            String a2 = args[1];
+//            String bee = a1 + " " + a2;
+//            System.Diagnostics.Process.Start("cmd.exe", bee);
+            
+        }
+    }
+}
